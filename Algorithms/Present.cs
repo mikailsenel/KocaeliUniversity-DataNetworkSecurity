a9c90ccbb695d506ce6c0e56370c1ef2c2916d24
Mehmet Akif Özdemir <89011948+Lu-ari@users.noreply.github.com>
2023-05-01 19:21:08 +0300

    Created By Mehmet Akif Özdemir

---
+++ Algorithms/Present.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Present
+    {
+
+
+       public void Initial(string input)
+        {
+            
+        }
+
+        
+
+
+
+}
+
+
+
+    }
+    
