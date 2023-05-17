e0d0e91d117cb7d7751ca0fce39d374c42624696
Mehmet Akif Özdemir <89011948+Lu-ari@users.noreply.github.com>
2023-05-17 15:42:36 +0300

    neokeon degistirildi

--- Algorithms/Noekeon.cs
+++ Algorithms/Noekeon.cs
@@ -11,25 +11,14 @@ namespace Algorithms;
 
 
 /*Algoritma tamamlanmıştır. Sağlıklı çalışmaktadır*/
-/*
-
-Substitution (Yerine Koyma):
-Substitution işlevi, sabit olarak tanımlanmış bir S-box'tan değerleri aratarak girdi baytlarını S-box'tan gelen karşılık gelen değerlerle değiştirerek yerine koyma adımını gerçekleştirir.
-permütasyon:
-Permütasyon işlevi, sabit bir permütasyon kuralları kümesine dayalı olarak dört adet 32 bitlik kelimenin sırasını karıştırarak permütasyon adımını uygular.
-
-Anahtar Ekleme:
-XOR ve Toplama işlevleri, temel toplama adımını uygular. Giriş sözcükleri ile orijinal anahtardan türetilen bir anahtar arasında (sırasıyla) bir XOR işlemi ve bir toplama işlemi gerçekleştirirler.
 
 
-*/
-
-public class Noekeon: EncryptionAlgorithm
+public class Noekeon : EncryptionAlgorithm
 {
     public Noekeon(string text) : base(text)
     {
     }
-public  string GetByteArrayAsBinaryString(byte[] byteArray)
+    public string GetByteArrayAsBinaryString(byte[] byteArray)
     {
         string binaryString = "";
 
@@ -43,144 +32,181 @@ public  string GetByteArrayAsBinaryString(byte[] byteArray)
     }
     protected override void Initial(string input)
     {
-        const int MaxInputLength = 16; // 16 byte = 128 bit
-        // Ornek input data
-        byte[] inputData = new byte[] { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, }; //16 bit input data giriş yapılıyor
-        // 128 bit üzerinde veri girişi kontrolü
-        if (inputData.Length > MaxInputLength)
-        {
-            Console.WriteLine("Hata: Giriş metni 128 bit (16 byte) üzerinde olamaz.");
-            AddStep("Hata: Giriş metni 128 bit (16 byte) üzerinde olamaz.", BitConverter.ToString(inputData));
-            return;
-        }
-        AddStep("Girdi Metin datasi..: " , BitConverter.ToString(inputData));
-        Console.WriteLine("Girdi Metin datasi..: " + BitConverter.ToString(inputData));
-        AddStep("Girdi Binary datası Binary: " ,GetByteArrayAsBinaryString(inputData));
-         Console.WriteLine("Girdi Binary datası Binary: " + GetByteArrayAsBinaryString(inputData));
-        // Ornek key
-        uint[] key = new uint[] { 0x01234567, 0x89ABCDEF, 0xFEDCBA98, 0x76543210 }; //128 bit data
+        /* const int MaxInputLength = 16; // 16 byte = 128 bit
+         // Ornek input data
+         byte[] inputData = new byte[] { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, }; //16 bit input data giriş yapılıyor
+         // 128 bit üzerinde veri girişi kontrolü
+         if (inputData.Length > MaxInputLength)
+         {
+             Console.WriteLine("Hata: Giriş metni 128 bit (16 byte) üzerinde olamaz.");
+             AddStep("Hata: Giriş metni 128 bit (16 byte) üzerinde olamaz.", BitConverter.ToString(inputData));
+             return;
+         }
+         AddStep("Girdi Metin datasi..: " , BitConverter.ToString(inputData));
+         Console.WriteLine("Girdi Metin datasi..: " + BitConverter.ToString(inputData));
+         AddStep("Girdi Binary datası Binary: " ,GetByteArrayAsBinaryString(inputData));
+          Console.WriteLine("Girdi Binary datası Binary: " + GetByteArrayAsBinaryString(inputData));
+         // Ornek key
+         uint[] key = new uint[] { 0x01234567, 0x89ABCDEF, 0xFEDCBA98, 0x76543210 }; //128 bit data
 
-        // Yerine Koyma Islemi
-        byte[] outputData = Substitution(inputData);
-         AddStep("Yerine koyma islemi sonrasi data..: " , BitConverter.ToString(outputData));
-        Console.WriteLine("Yerine koyma islemi sonrasi data..: " + BitConverter.ToString(outputData));
-          AddStep("Yerine koyma islemi sonrasi data Binary: " , GetByteArrayAsBinaryString(outputData));
-         Console.WriteLine("Yerine koyma islemi sonrasi data Binary: " + GetByteArrayAsBinaryString(outputData));
-        // Permutasyon işlemi
-        uint a = BitConverter.ToUInt32(outputData, 0);
-        uint b = BitConverter.ToUInt32(outputData, 4);
-        uint c = BitConverter.ToUInt32(outputData, 8);
-        uint d = BitConverter.ToUInt32(outputData, 12);
+         // Yerine Koyma Islemi
+         byte[] outputData = Substitution(inputData);
+          AddStep("Yerine koyma islemi sonrasi data..: " , BitConverter.ToString(outputData));
+         Console.WriteLine("Yerine koyma islemi sonrasi data..: " + BitConverter.ToString(outputData));
+           AddStep("Yerine koyma islemi sonrasi data Binary: " , GetByteArrayAsBinaryString(outputData));
+          Console.WriteLine("Yerine koyma islemi sonrasi data Binary: " + GetByteArrayAsBinaryString(outputData));
+         // Permutasyon işlemi
+         uint a = BitConverter.ToUInt32(outputData, 0);
+         uint b = BitConverter.ToUInt32(outputData, 4);
+         uint c = BitConverter.ToUInt32(outputData, 8);
+         uint d = BitConverter.ToUInt32(outputData, 12);
 
 
-        Permutation(ref a, ref b, ref c, ref d);
-        outputData = BitConverter.GetBytes(a).Concat(BitConverter.GetBytes(b)).Concat(BitConverter.GetBytes(c)).Concat(BitConverter.GetBytes(d)).ToArray();
-       AddStep("Permutasyon sonrasi data : " , BitConverter.ToString(outputData));
-        Console.WriteLine("Permutasyon sonrasi data : " + BitConverter.ToString(outputData));
-        AddStep("Permutasyon sonrasi data Binary: " , GetByteArrayAsBinaryString(outputData));
-        Console.WriteLine("Permutasyon sonrasi data Binary: " + GetByteArrayAsBinaryString(outputData));
-        //  XOR ve Toplama Islemi
-        a = BitConverter.ToUInt32(outputData, 0);
-        b = BitConverter.ToUInt32(outputData, 4);
-        c = BitConverter.ToUInt32(outputData, 8);
-        d = BitConverter.ToUInt32(outputData, 12);
+         Permutation(ref a, ref b, ref c, ref d);
+         outputData = BitConverter.GetBytes(a).Concat(BitConverter.GetBytes(b)).Concat(BitConverter.GetBytes(c)).Concat(BitConverter.GetBytes(d)).ToArray();
+        AddStep("Permutasyon sonrasi data : " , BitConverter.ToString(outputData));
+         Console.WriteLine("Permutasyon sonrasi data : " + BitConverter.ToString(outputData));
+         AddStep("Permutasyon sonrasi data Binary: " , GetByteArrayAsBinaryString(outputData));
+         Console.WriteLine("Permutasyon sonrasi data Binary: " + GetByteArrayAsBinaryString(outputData));
+         //  XOR ve Toplama Islemi
+         a = BitConverter.ToUInt32(outputData, 0);
+         b = BitConverter.ToUInt32(outputData, 4);
+         c = BitConverter.ToUInt32(outputData, 8);
+         d = BitConverter.ToUInt32(outputData, 12);
 
-        XOR(ref a, ref b, ref c, ref d, key);
-        byte[] xordata = BitConverter.GetBytes(a).Concat(BitConverter.GetBytes(b)).Concat(BitConverter.GetBytes(c)).Concat(BitConverter.GetBytes(d)).ToArray();
-       
-       AddStep("Xor sonrasi data: " , BitConverter.ToString(xordata));
-        Console.WriteLine("Xor sonrasi data: " + BitConverter.ToString(xordata));
-          AddStep("Xor sonrasi data Binary: " , GetByteArrayAsBinaryString(xordata));
-        Console.WriteLine("Xor sonrasi data Binary: " + GetByteArrayAsBinaryString(xordata));
+         XOR(ref a, ref b, ref c, ref d, key);
+         byte[] xordata = BitConverter.GetBytes(a).Concat(BitConverter.GetBytes(b)).Concat(BitConverter.GetBytes(c)).Concat(BitConverter.GetBytes(d)).ToArray();
 
-        Addition(ref a, ref b, ref c, ref d, key);
-        byte[] addition = BitConverter.GetBytes(a).Concat(BitConverter.GetBytes(b)).Concat(BitConverter.GetBytes(c)).Concat(BitConverter.GetBytes(d)).ToArray();
-        AddStep("Toplama sonrasi data: " , BitConverter.ToString(addition));
-        Console.WriteLine("Toplama sonrasi data: " + BitConverter.ToString(addition));
-        AddStep("Toplama sonrasi data Binary: " , GetByteArrayAsBinaryString(addition));
-        Console.WriteLine("Toplama sonrasi data Binary: " + GetByteArrayAsBinaryString(addition));
-        // Cıktı sonucu
+        AddStep("Xor sonrasi data: " , BitConverter.ToString(xordata));
+         Console.WriteLine("Xor sonrasi data: " + BitConverter.ToString(xordata));
+           AddStep("Xor sonrasi data Binary: " , GetByteArrayAsBinaryString(xordata));
+         Console.WriteLine("Xor sonrasi data Binary: " + GetByteArrayAsBinaryString(xordata));
 
-        AddStep(" Sifrelenmiş data..: " , BitConverter.ToString(outputData));
-        Console.WriteLine(" Sifrelenmiş data..: " + BitConverter.ToString(outputData));
-         AddStep("Sifrelenmiş data Binary: " , GetByteArrayAsBinaryString(outputData));
-        Console.WriteLine("Sifrelenmiş data Binary: " + GetByteArrayAsBinaryString(outputData));
+         Addition(ref a, ref b, ref c, ref d, key);
+         byte[] addition = BitConverter.GetBytes(a).Concat(BitConverter.GetBytes(b)).Concat(BitConverter.GetBytes(c)).Concat(BitConverter.GetBytes(d)).ToArray();
+         AddStep("Toplama sonrasi data: " , BitConverter.ToString(addition));
+         Console.WriteLine("Toplama sonrasi data: " + BitConverter.ToString(addition));
+         AddStep("Toplama sonrasi data Binary: " , GetByteArrayAsBinaryString(addition));
+         Console.WriteLine("Toplama sonrasi data Binary: " + GetByteArrayAsBinaryString(addition));
+         // Cıktı sonucu
+
+         AddStep(" Sifrelenmiş data..: " , BitConverter.ToString(outputData));
+         Console.WriteLine(" Sifrelenmiş data..: " + BitConverter.ToString(outputData));
+          AddStep("Sifrelenmiş data Binary: " , GetByteArrayAsBinaryString(outputData));
+         Console.WriteLine("Sifrelenmiş data Binary: " + GetByteArrayAsBinaryString(outputData));*/
+
+        string inputText = input;
+        string key = "mysecretkey12345";
+
+        Noekeon noekeon = new Noekeon(inputText);
+        string encryptedText = noekeon.Encrypt(inputText, key);
+
+        Console.WriteLine("Girdi Metin datasi..: " + inputText);
+        AddStep("Girdi Metin datasi..: ", inputText);
+        Console.WriteLine("Şifrelenmiş Text: " + encryptedText);
+        AddStep("Girdi Metin datasi..: ", encryptedText);
+
+
     }
 
-    public byte[] Substitution(byte[] input)
+    private const uint Delta = 0x9e3779b9;
+    private const int Rounds = 16;
+    private uint[] roundKeys;
+
+    public string Encrypt(string input, string key)
     {
-        byte[] output = new byte[input.Length];
-        byte[] sbox = { // Yerine koyma işlemi arama tablosu 16 * 16 256 bit sbox datası vardır
-        0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x0, 0x1, 0x2, 0x3, 0x4,
-        0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9,
-        0xF, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE,
-        0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x0, 0x1, 0x2, 0x3,
-        0xE, 0xF, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD,
-        0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x0, 0x1, 0x2,
-        0xD, 0xE, 0xF, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC,
-        0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x0, 0x1,
-        0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x0, 0x1, 0x2, 0x3, 0x4,
-        0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9,
-        0xF, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE,
-        0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x0, 0x1, 0x2, 0x3,
-        0xE, 0xF, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD,
-        0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x0, 0x1, 0x2,
-        0xD, 0xE, 0xF, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC,
-        0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x0, 0x1
-    };
-        for (int i = 0; i < input.Length; i++)
+        if (key.Length != 16)
         {
-            output[i] = sbox[input[i]];
-        }
-        return output;
-    }
-    public void Permutation(ref uint a, ref uint b, ref uint c, ref uint d)
-    {
-        const uint delta = 0x9E3779B9; // Altin oran
-        uint tmp;
-        for (int i = 0; i < 4; i++)
-        {
-            // Theta Adimi
-            a += c; c ^= b; c = (c << 16) | (c >> 16);
-            b += d; d ^= a; d = (d << 12) | (d >> 20);
-            a += b; b ^= c; b = (b << 8) | (b >> 24);
-            c += d; d ^= a; d = (d << 7) | (d >> 25);
-
-            // Pi adimi
-            tmp = a; a = b; b = c; c = d; d = tmp;
-
-            // Gamma adimi
-            a = (a << 1) | (a >> 31);
-            b = (b << 2) | (b >> 30);
-            c = (c << 3) | (c >> 29);
-            d = (d << 5) | (d >> 27);
-
-            // Theta adimi
-            a += c; c ^= b; c = (c << 16) | (c >> (32 - 16));
-            b += d; d ^= a; d = (d << 12) | (d >> 20);
-            a += b; b ^= c; b = (b << 8) | (b >> 24);
-            c += d; d ^= a; d = (d << 7) | (d >> 25);
-
-            // Key zamanlama
-            a ^= delta;
+            throw new ArgumentException("Key length should be 16 characters.");
         }
 
+        roundKeys = GenerateRoundKeys(key);
+        //eklendi
+        uint[] block = new uint[4];
+        byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+        int inputLength = inputBytes.Length;
+        int outputLength = (inputLength / 16 + 1) * 16;
+        byte[] outputBytes = new byte[outputLength];
+
+        for (int i = 0; i < inputLength; i += 16)
+        {
+            for (int j = 0; j < 16; j++)
+            {
+                if (i + j < inputLength)
+                {
+                    block[j / 4] |= (uint)(inputBytes[i + j] << (8 * (j % 4)));
+                }
+            }
+
+            EncryptBlock(block);
+
+            for (int j = 0; j < 16; j++)
+            {
+                if (i + j < inputLength)
+                {
+                    outputBytes[i + j] = (byte)(block[j / 4] >> (8 * (j % 4)));
+                }
+            }
+        }
+
+        return System.Text.Encoding.UTF8.GetString(outputBytes);
     }
 
-    public void XOR(ref uint a, ref uint b, ref uint c, ref uint d, uint[] key) //xor fonksiyonu
+    private void EncryptBlock(uint[] block)
     {
-        a ^= key[0];
-        b ^= key[1];
-        c ^= key[2];
-        d ^= key[3];
-    }
-    public void Addition(ref uint a, ref uint b, ref uint c, ref uint d, uint[] key) //toplama fonksiyonu
-    {
-        const uint constant = 0x08070605;
-        a += constant;
-        b += key[0];
-        c += key[1];
-        d += key[2];
+        uint x0 = block[0];
+        uint x1 = block[1];
+        uint x2 = block[2];
+        uint x3 = block[3];
+
+        for (int round = 0; round < Rounds; round++)
+        {
+            uint temp = x3;
+            x3 = x2 ^ ((x1 & (x0 ^ x2)) ^ x0 ^ roundKeys[round]);
+            x2 = x1;
+            x1 = x0;
+            x0 = temp;
+        }
+
+        block[0] = x0;
+        block[1] = x1;
+        block[2] = x2;
+        block[3] = x3;
     }
 
+    private uint[] GenerateRoundKeys(string key)
+    {
+        uint[] keyWords = new uint[4];
+
+        for (int i = 0; i < 16; i += 4)
+        {
+            keyWords[i / 4] = (uint)(key[i] << 24) |
+                             (uint)(key[i + 1] << 16) |
+                             (uint)(key[i + 2] << 8) |
+                             (uint)(key[i + 3]);
+        }
+
+        uint[] roundKeys = new uint[Rounds];
+
+        roundKeys[0] = keyWords[0];
+        roundKeys[1] = keyWords[1];
+        roundKeys[2] = keyWords[2];
+        roundKeys[3] = keyWords[3];
+
+        for (int i = 4; i < Rounds; i++)
+        {
+            roundKeys[i] = roundKeys[i - 4] ^ RotateLeft(roundKeys[i - 1], 3) ^ Delta ^ (uint)i;
+        }
+
+        return roundKeys;
+    }
+
+    private uint RotateLeft(uint value, int count)
+    {
+        return (value << count) | (value >> (32 - count));
+    }
 }
+
+
+
+
