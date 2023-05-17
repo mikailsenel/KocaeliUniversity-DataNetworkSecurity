125e291721a1da29349ae2bc250a161751333787
Mikail Şenel <mikail.snl@gmail.com>
2023-05-17 20:50:13 +0300

    düzeltme

--- Algorithms/Piccolo.cs
+++ Algorithms/Piccolo.cs
@@ -138,7 +138,7 @@ namespace Algorithms
 
         return plaintext;
     }
-        protected override void Initial(string input)
+        protected override void Initial(string input, string inputKey)
         {
             /*const int MaxInputLength = 16; // 16 byte = 128 bit
             byte[] key = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
